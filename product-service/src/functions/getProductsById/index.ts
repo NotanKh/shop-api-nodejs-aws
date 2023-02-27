1413cb000d356652e690817b4b00c27a96f63896
Anton Khranovskii <anton_khranovskiiepam.com>
2023-02-27 18:01:59 +0300

    [Task 6] serverless refactoring

--- product-service/src/functions/getProductsById/index.ts
+++ product-service/src/functions/getProductsById/index.ts
@@ -2,11 +2,6 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export const getProductByIdFunction = {
   handler: `${handlerPath(__dirname)}/handler.main`,
-  environment: {
-    /* eslint-disable no-template-curly-in-string */
-    PRODUCTS_TABLE_NAME: '${self:custom.productsTableName}',
-    STOCK_TABLE_NAME: '${self:custom.stockTableName}',
-  },
   events: [
     {
       http: {
